0c183f6f14f3d2c9436ac5ac8277d2cdf17f6fd7
israel <israel.bejarano.a@gmail.com>
2019-12-20 15:40:53 +0100

    joinTable

--- springboot-servicio-usuarios/src/main/java/com/microservicios/springboot/app/usuarios/models/entity/Usuario.java
+++ springboot-servicio-usuarios/src/main/java/com/microservicios/springboot/app/usuarios/models/entity/Usuario.java
@@ -9,8 +9,11 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "usuarios")
@@ -36,6 +39,9 @@ public class Usuario implements Serializable {
 	private String email;
 	
 	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "usuarios_roles", joinColumns = @JoinColumn(name="usuario_id"),
+	inverseJoinColumns = @JoinColumn(name="role_id"),
+	uniqueConstraints = {@UniqueConstraint(columnNames ={"usuario_id", "role_id"})})
 	private List<Role> roles;
 
 	public List<Role> getRoles() {
