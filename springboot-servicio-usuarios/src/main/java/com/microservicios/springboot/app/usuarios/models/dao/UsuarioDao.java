cf7e70f9bd717e6a747397af1cf62ba0fe2a1143
israel <israel.bejarano.a@gmail.com>
2019-12-20 16:16:10 +0100

    Implementación un repositorio rest para el CRUD

--- springboot-servicio-usuarios/src/main/java/com/microservicios/springboot/app/usuarios/models/dao/UsuarioDao.java
+++ springboot-servicio-usuarios/src/main/java/com/microservicios/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -2,9 +2,11 @@ package com.microservicios.springboot.app.usuarios.models.dao;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.microservicios.springboot.app.usuarios.models.entity.Usuario;
 
+@RepositoryRestResource(path = "usuarios")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long> {
 	
 	public Usuario findByUsername(String username);
