dc81eebed322a170fb7000129bf33761c794dcd9
israel <israel.bejarano.a@gmail.com>
2019-12-20 16:50:17 +0100

    uso de lib commons usuarios

--- springboot-servicio-usuarios/src/main/java/com/microservicios/springboot/app/usuarios/RepositoryConfig.java
+++ springboot-servicio-usuarios/src/main/java/com/microservicios/springboot/app/usuarios/RepositoryConfig.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
-import com.microservicios.springboot.app.usuarios.models.entity.Role;
-import com.microservicios.springboot.app.usuarios.models.entity.Usuario;
+import com.microservicios.springboot.app.usuarios.commons.models.entity.Role;
+import com.microservicios.springboot.app.usuarios.commons.models.entity.Usuario;
 
 @Configuration
 public class RepositoryConfig implements RepositoryRestConfigurer {
