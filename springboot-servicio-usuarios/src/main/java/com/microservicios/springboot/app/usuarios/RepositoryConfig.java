427fc31e806a4dc29015869488c69d60833941fc
israel <israel.bejarano.a@gmail.com>
2019-12-20 16:28:17 +0100

    Configurando y exponiendo id en la respuesta json

---
+++ springboot-servicio-usuarios/src/main/java/com/microservicios/springboot/app/usuarios/RepositoryConfig.java
@@ -0,0 +1,18 @@
+package com.microservicios.springboot.app.usuarios;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import com.microservicios.springboot.app.usuarios.models.entity.Role;
+import com.microservicios.springboot.app.usuarios.models.entity.Usuario;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Usuario.class, Role.class);
+	}
+
+}
